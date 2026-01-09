74b6b0fd5359c2d927fd321eb4c4e1962eb5cd79
shubham9756 <skarad819@gmail.com>
2026-01-09 11:36:46 +0530

    Update Blog.jsx

--- src/Components/User/Blog.jsx
+++ src/Components/User/Blog.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./css/Blog.css";
+import "./css/blog.css";
 
 const Blog = () => {
 
